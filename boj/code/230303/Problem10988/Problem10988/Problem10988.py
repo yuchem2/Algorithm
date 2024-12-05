6a743a0c482e823c571686c6cba8b6cb16848b4d
Jaehyun Yoon <yuchem2@gmail.com>
2024-12-05 13:41:34 +0900

    Delete boj/code directory

--- boj/code/230303/Problem10988/Problem10988/Problem10988.py
+++
@@ -1,3 +0,0 @@
-
-inStr = input()
-print(1) if list(inStr) == list(reversed(inStr)) else print(0)
--- boj/code/230303/Problem3003/Problem3003/Problem3003.py
+++
@@ -1,9 +0,0 @@
-need_data = [1, 1, 2, 2, 2, 8]
-out_data = [0, 0, 0, 0, 0, 0]
-x = input()
-in_data = x.split()
-
-for i in range(0, len(need_data), 1):
-    out_data[i] = need_data[i] - int(in_data[i])
-    print(out_data[i], end = " ")
-print()
